301e48b4d7ca2f703cfe6c1ccf720209bf60b847
Angel Heredia <angl.vaz6199@gmail.com>
2021-05-31 16:42:38 -0500

    Host Field correction and turn settings to production

--- Movies_MongoDB/wsgi.py
+++ Movies_MongoDB/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Movies_MongoDB.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Movies_MongoDB.settings.production')
 
 application = get_wsgi_application()
